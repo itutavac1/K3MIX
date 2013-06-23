58008d738ccf10a7b764fbdbed2bea717508e437
itutavac <itutavac@foi.hr>
2013-06-24 01:33:06 +0200

    Test

---
+++ K3MIX/Korisnici1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.EntityModel;
+
+namespace K3MIX
+{
+    public partial class Korisnici
+    {
+        /// <summary>
+        /// User instance, consumer will be null
+        /// </summary>
+        /// <param name="x">Does nothing</param>
+        public Korisnici(int x)
+        {
+           this.Dodatni_podaci_konzumenta = null;
+           this.Dodatni_podaci_korisnika = new HashSet<Dodatni_podaci_korisnika>();
+           this.Pracenje_po_datumima = new HashSet<Pracenje_po_datumima>();
+           this.Pracenje_po_datumima1 = new HashSet<Pracenje_po_datumima>();
+        }
+    }
+}
