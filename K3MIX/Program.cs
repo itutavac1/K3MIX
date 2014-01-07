692b3be2ea86ce5c065dadfe4d4fc38a2b0ae81d
IvanTutavac <itutavac1@gmail.com>
2014-01-07 19:27:42 +0100

    Reset

--- K3MIX/Program.cs
+++ K3MIX/Program.cs
@@ -4,24 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-/*  Naming, declaration, and stuff like that ...
-    class CName;  
-    CName Name;   
-    Name->Init(); 
-    int firstSecondThird = 0;
-    void FirstSecondThird();
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            if (!something)
-            {
-                // try again
-            }
-        }
-    }
-*/
-
 namespace K3MIX
 {
     static class Program
@@ -34,10 +16,8 @@ namespace K3MIX
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            CApp App = new CApp();
-            if (!App.Init())
-                return;
-            Application.Run(new Form1());
+           
+            Application.Run(new frmLogin());
         }
     }
 }
--- K3MIX/frmLogin.Designer.cs
+++ K3MIX/frmLogin.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace K3MIX
 {
-    partial class FTemp
+    partial class frmLogin
     {
         /// <summary>
         /// Required designer variable.
@@ -30,7 +30,7 @@
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "FormTemp";
+            this.Text = "frmLogin";
         }
 
         #endregion
